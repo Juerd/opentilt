c85ca01eac653cef8cff47c75350bfd92e934f0c
WinSCaP <site-github.com@lachi.nl>
2013-09-21 23:56:46 +0200

    Broadcast messages are now send an extra 3 times with 3ms delay.

--- opentilt1.ino
+++ opentilt1.ino
@@ -75,13 +75,11 @@ void pin2_isr() {
 void power_down() {
     AGAIN:
     wdt_disable();
-    sleep_enable();    cli(); //sleep_bod_disable(); 
-    sei();
+    sleep_enable();
     attachInterrupt(0, pin2_isr, LOW);
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     ADCSRA |= (0<<ADEN);  // disable ADC
-    cli(); //sleep_bod_disable(); 
-    sei();
+    cli(); sleep_bod_disable(); sei();
     sleep_cpu();
     sleep_disable();
     ADCSRA |= (1<<ADEN);  // enable ADC
@@ -180,10 +178,10 @@ bool send(long int destination, uint8_t msg, union Param param) {
     bool ok = rf.write(&p, sizeof(p));
     // If packet is a broadcast, send another 9 times with 5ms delay
     if (destination == broadcast) {
-      for (int broadcast_nr = 0; broadcast_nr < 2; broadcast_nr++) {
-        delay(3);
-        bool ok = rf.write(&p, sizeof(p));
-      }    
+        for (int broadcast_nr = 0; broadcast_nr < 2; broadcast_nr++) {
+            delay(3);
+            bool ok = rf.write(&p, sizeof(p));
+        }    
     } 
     rf.openReadingPipe(0, broadcast);
     rf.startListening();
