11ed3a34ab14c2cca4194ea66464888da46d8a2a
Juerd Waalboer <juerd@tnx.nl>
2013-09-27 21:12:31 +0200

    CR before LF

--- debug.h
+++ debug.h
@@ -3,7 +3,7 @@
 #ifdef DEBUG
     int debug_putc(char c, FILE *) { Serial.write(c); return c; }
     void debug_begin(void) { fdevopen(&debug_putc, 0); }
-    #define D(f, ...) do { printf_P(PSTR(f "\n"), ##__VA_ARGS__); } while (0)
+    #define D(f, ...) do { printf_P(PSTR(f "\r\n"), ##__VA_ARGS__); } while (0)
 #else
     #define debug_begin()
     #define D(...)
